5f50a429e70d0f902424587e7243d5cda8c2c8a0
quoctrungdh <quoctrungdh@gmail.com>
2017-09-10 22:45:54 +0700

    feat(herokuDeployment): adding Profile

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const index = require('./routers/');
 const login = require('./routers/login.js');
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
