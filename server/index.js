654ca838957814c8fb2e3d8564a48920c90762e1
quoctrungdh <quoctrungdh@gmail.com>
2017-09-12 01:50:00 +0700

    feat(cors): add cors for cross domain

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const index = require('./routers/');
 const login = require('./routers/login.js');
 
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
